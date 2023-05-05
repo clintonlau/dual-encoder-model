57736ec30862ba5a4d8e7438db97db9b3ea3c72b
Clinton Lau <lau.clinton@gmail.com>
2023-05-05 11:27:27 -0400

    Create function for dataset init

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -2,6 +2,8 @@ from os import listdir
 from os.path import isfile, join
 import pandas as pd
 from natsort import natsorted
+import torch
+from torch.utils.data import Dataset
 
 def create_input_df(dir, label_data):
     """
@@ -51,3 +53,33 @@ def transcripts_to_list(file_paths):
         resp_timestamps.append(response_timestamp)
 
     return subject_ids, text_inputs, resp_timestamps
+
+def init_text_datasets(train_df, dev_df):
+    return DaicTextDataset(train_df), DaicTextDataset(dev_df)
+
+class DaicTextDataset(Dataset):
+    def __init__(self, df):
+        self.df = df
+
+        self.subject_ids = self.df['subject_id'].to_list()
+        self.text_input = self.df['text_input'].to_list()
+        self.binary_labels = self.df['PHQ8_Binary'].to_list()
+        self.score_labels = self.df['PHQ8_Score'].to_list()
+
+    def __getitem__(self, idx):
+        text_input = self.text_input[idx]
+        subject_id = self.subject_ids[idx]
+        binary_label = torch.tensor(self.binary_labels[idx], dtype=torch.float32)
+        score_label = torch.tensor(self.score_labels[idx], dtype=torch.float32)
+    
+        sample = {
+            'subject_id': subject_id,
+            'text_input': text_input, 
+            'binary_label': binary_label,
+            'score_label': score_label,
+        }
+
+        return sample
+
+    def __len__(self):
+        return len(self.subject_ids)
